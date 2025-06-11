3a869afba095d1f00be519af2d0071cdb205d081
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-11 19:18:03 +0900

    feat: add UI directory

---
+++ Assets/Scripts/UI/StartScene/StartButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    private void Start()
+    {
+        if(this.gameObject.TryGetComponent<Button>(out Button button))
+        {
+            button.onClick.AddListener(() => SceneManager.LoadSceneAsync("SelectSaveScene"));
+        }
+    }
+}
