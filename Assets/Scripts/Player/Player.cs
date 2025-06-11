6ea0ba54aae85797938f66f7de4796c41723c667
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-12 06:46:00 +0900

    feat: add test code

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,5 +12,12 @@ public class Player : APlayer
     protected override void Update()
     {
         base.Update();
+        /*float horizontal = Input.GetAxis("Horizontal"); // A/D or ←/→
+        float vertical = Input.GetAxis("Vertical");     // W/S or ↑/↓
+
+        Vector3 move = new Vector3(horizontal, vertical);
+
+        // 프레임별로 이동량 계산
+        transform.position += move * 5f * Time.deltaTime;*/
     }
 }
