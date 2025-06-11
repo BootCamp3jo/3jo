36422d17901ed3ef120d4ae567b014f10d8b8fc5
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-12 06:35:22 +0900

    feat: add abstract class for player

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : APlayer
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
