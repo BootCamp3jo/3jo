3b65dd1378a9151041c23a1c347f31d61e71c3ae
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-12 09:09:28 +0900

    Update ANPC.cs

--- Assets/Scripts/NPC/ANPC.cs
+++ Assets/Scripts/NPC/ANPC.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ANPC : MonoBehaviour
+public abstract class ANPC : MonoBehaviour
 {
     [SerializeField]
     public NPCData npcData;
