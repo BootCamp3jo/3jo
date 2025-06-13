6faefafe490f8ffbe15835d06a1a7d0ce1c1fafd
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-13 20:32:13 +0900

    feat: merge

--- Assets/Scripts/Player/PlayerParents/APlayer.cs
+++ Assets/Scripts/Player/PlayerParents/APlayer.cs
@@ -2,7 +2,7 @@ using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public abstract class PlayerSaver : MonoBehaviour
+public abstract class APlayer : MonoBehaviour
 {
     [SerializeField]
     protected PlayerData playerData;
