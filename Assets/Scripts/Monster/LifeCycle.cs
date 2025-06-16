7104dac4f9aa40a046161c72108b4e7eabb502c8
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2025-06-16 19:35:39 +0900

    revert : 불릿 타임 관련 함수 리버트
    - 리버트

--- Assets/Scripts/Monster/LifeCycle.cs
+++ Assets/Scripts/Monster/LifeCycle.cs
@@ -1,37 +1,17 @@
 using UnityEngine;
 
-public class LifeCycle : MonoBehaviour, ISlowAble
+public class LifeCycle : MonoBehaviour
 {
-    // 지속 시간
-    public float lifeTime { get; set; }
+    // 기본 1초
+    public float lifeTime { get; set; }  = 1f;
 
-    // 지속 시간 빼주기! >> 슬로우가 되면 줄어드는 속도도 감소
-    public void Update()
+    protected virtual void OnEnable()
     {
-        if(isSlowed)
-            lifeTime -= GameManager.Instance.slowRate * Time.deltaTime;
-        else
-            lifeTime -= Time.deltaTime;
-        if (lifeTime <= 0)
-            LifeEnd();
+        Invoke("LifeEnd", lifeTime);
     }
 
     public virtual void LifeEnd()
     {
         gameObject.SetActive(false);
-    }
-
-    // 슬로우/해제 관련 구현
-    // 슬로우 중인지 판정하기 위한 프로퍼티(없으면 감속이 된 건지 아닌지 판정할 수 없기에)
-    public bool isSlowed { get; set; } = false;
-    // 감속 시작!
-    public void StartSlow()
-    {
-        isSlowed = true;
-    }
-    // 감속 끝!
-    public void StopSlow()
-    {
-        isSlowed = false;
     }
 }
