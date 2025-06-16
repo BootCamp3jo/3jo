d3874ea83b4503bea6ea20877a842ef7bba1ac8b
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2025-06-16 15:18:56 +0900

    feat : 로비씬 NPC 추가
    - 로비씬 NPC 추가

---
+++ Assets/Scripts/NPC/DialogueNpc.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace npcDialogue
+{
+    enum DialogueKey
+    {
+        Near,
+        Click,
+    }
+
+    public class DialogueNpc : BaseNpc
+    {
+        [SerializeField] private DialogueData _dialogueData;
+        private NpcDialogueController _dialogueController;
+
+        private void Awake()
+        {
+            _dialogueController = GetComponent<NpcDialogueController>();
+        }
+
+        public override void InteractiveNPC()
+        {
+            base.InteractiveNPC();
+            ShowTextBox(DialogueKey.Click.ToString());
+        }
+
+        public void ShowTextBox(string key)
+        {
+            var lines = GetDialogueLinesByKey(key);
+            if (lines != null && lines.Count > 0)
+            {
+                _dialogueController.ShowLines(lines);
+            }
+        }
+
+        List<string> GetDialogueLinesByKey(string key)
+        {
+            return _dialogueData.data.FirstOrDefault(d => d.key == key)?.texts;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("PlayerOnly"))
+            {
+                ShowTextBox(DialogueKey.Near.ToString());
+            }
+        }
+    }
+}
